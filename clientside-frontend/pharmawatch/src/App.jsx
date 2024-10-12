be6d6e169d478dc9b6e97854f64cac80e18c6a2d
JeffCao0801 <hermitjeff888@gmail.com>
2024-10-12 03:38:29 -0400

    initial template

--- clientside-frontend/pharmawatch/src/App.jsx
+++ clientside-frontend/pharmawatch/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app">
+        <div>
+          <div className="nav">
+            <div>PHARMAWATCH</div>
+          </div>
+          <div className="main-body">
+            <div className="left-section">
+              <div className="left-section-selector">Calendar</div>
+              <div className="left-section-selector">Dashboard</div>
+              <div className="left-section-checklist-container">check list placeholder</div>
+              {/* have a map that returns rows of them */}
+            </div>
+            <div className="right-section">
+              <div className="right-top-section">
+                <div className="right-top-row"></div>
+                <div className="right-bottow-row"></div>
+              </div>
+              <div className="info section">
+                 <div className="appointment-history">appot hist</div>
+                 <div className="perscription-histoty">per his</div>
+                 <div className="summery-graph">summery g</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
